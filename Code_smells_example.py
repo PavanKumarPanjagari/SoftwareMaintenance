5c7787ade0ca0263c3c12865e5df7ad9f53de63d
PavanKumarPanjagari <143746105+PavanKumarPanjagari@users.noreply.github.com>
2023-10-30 12:26:45 -0700

    Update Code_smells_example.py

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,14 +1,13 @@
 def calculate_total_price(items):
     total = 0
-
-    def calculate_item_price(item):
-        if item.price > 100:
-            return item.price * 0.9
-        else:
-            return item.price * 0.95
-
     for item in items:
-        if item.quantity > 0:
-            total += calculate_item_price(item)
-
+        total += calculate_item_price(item)
     return total
+
+def calculate_item_price(item):
+    if item.quantity <= 0:
+        return 0  # Quantity is not positive, so the item is not included in the total
+    elif item.price > 100:
+        return item.price * 0.9  # Apply a 10% discount
+    else:
+        return item.price * 0.95  # Apply a 5% discount
